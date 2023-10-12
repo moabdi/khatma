fc718b2e2fed09d138e13e7f49f55caa62706322
moabdi <abdimostapha@gmail.com>
2023-10-12 22:55:10 +0200

    fix error

--- lib/src/features/khatma/utils/image_utils.dart
+++ lib/src/features/khatma/utils/image_utils.dart
@@ -1,5 +1,3 @@
-import 'package:khatma/src/features/khatma/domain/khatma.dart';
-
 class ImageUtils {
   static String getAssetImage(String? type) {
     // if (type == null || KhatmaType.custom.name == type) {
