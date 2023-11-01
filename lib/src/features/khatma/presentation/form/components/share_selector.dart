5c2d665344a1d4ab615635d3660573dd7967fece
moabdi <abdimostapha@gmail.com>
2023-11-01 13:06:53 +0100

    add hight

--- lib/src/features/khatma/presentation/form/components/share_selector.dart
+++ lib/src/features/khatma/presentation/form/components/share_selector.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:khatma/src/common/constants/app_sizes.dart';
 import 'package:khatma/src/common/utils/common.dart';
 import 'package:khatma/src/common/widgets/radio_icon.dart';
 import 'package:khatma/src/themes/theme.dart';
@@ -19,19 +20,24 @@ class ShareSelector extends StatelessWidget {
       Icon(Icons.public, color: Colors.blue),
       Icon(Icons.group, color: Colors.purple),
     ];
-    return ListView.separated(
-      separatorBuilder: (context, index) => const SizedBox(),
-      shrinkWrap: true,
-      itemCount: KhatmaShareType.values.length,
-      itemBuilder: (BuildContext context, int index) {
-        var currentUnit = KhatmaShareType.values[index];
-        var selected = unit == currentUnit;
-        return ShareTile(
-            selected: selected,
-            currentUnit: currentUnit,
-            icon: icons[index],
-            onSelect: onSelect);
-      },
+    return Column(
+      children: [
+        ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(),
+          shrinkWrap: true,
+          itemCount: KhatmaShareType.values.length,
+          itemBuilder: (BuildContext context, int index) {
+            var currentUnit = KhatmaShareType.values[index];
+            var selected = unit == currentUnit;
+            return ShareTile(
+              selected: selected,
+              currentUnit: currentUnit,
+              onSelect: onSelect,
+            );
+          },
+        ),
+        gapH24,
+      ],
     );
   }
 }
@@ -41,13 +47,11 @@ class ShareTile extends StatelessWidget {
     super.key,
     required this.selected,
     required this.currentUnit,
-    required this.icon,
     required this.onSelect,
   });
 
   final bool selected;
   final KhatmaShareType currentUnit;
-  final Icon icon;
   final ValueChanged<KhatmaShareType> onSelect;
 
   @override
