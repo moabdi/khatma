a10821f534042f1ec561c7206af90fb73803af9c
moabdi <abdimostapha@gmail.com>
2023-11-12 13:19:09 +0100

    Remove unused imports in share_selector and khatma_form_screen

--- lib/src/features/khatma/presentation/form/khatma_form_screen.dart
+++ lib/src/features/khatma/presentation/form/khatma_form_screen.dart
@@ -1,4 +1,3 @@
-import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:khatma/src/common/constants/app_sizes.dart';
